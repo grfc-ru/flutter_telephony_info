6f62db7a3e7346dd02f386e83c0771f35c67e0e9
leech001 <leech001@gmail.com>
2023-03-19 12:30:34 +0300

    added telephony code

--- android/src/main/kotlin/ru/grfc/telephony/flutter_telephony_info/FlutterTelephonyInfoPlugin.kt
+++ android/src/main/kotlin/ru/grfc/telephony/flutter_telephony_info/FlutterTelephonyInfoPlugin.kt
@@ -1,35 +1,66 @@
 package ru.grfc.telephony.flutter_telephony_info
 
-import androidx.annotation.NonNull
-
+import android.app.Activity
+import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
+import android.telephony.TelephonyManager
+import android.telephony.cdma.CdmaCellLocation
+import android.telephony.gsm.GsmCellLocation
 
 /** PigeonPlugin */
-class FlutterTelephonyInfoPlugin: FlutterPlugin, Telephony.TelephonyAPI {
-    private lateinit var channel : MethodChannel
+class FlutterTelephonyInfoPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Telephony.TelephonyAPI {
+    private lateinit var channel: MethodChannel
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    private lateinit var context: Context
+    private lateinit var activity: Activity
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         Telephony.TelephonyAPI.setup(flutterPluginBinding.binaryMessenger, this)
+        context = flutterPluginBinding.applicationContext
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         Telephony.TelephonyAPI.setup(binding.binaryMessenger, null)
     }
 
-    override fun getInfo(): List<Telephony.TelephonyInfo>? {
-        val result: List<Telephony.TelephonyInfo>
+    override fun getInfo(): List<Telephony.TelephonyInfo> {
+        var mTelephonyManager: TelephonyManager? = null
 
-        var t1= Telephony.TelephonyInfo()
-        var t2= Telephony.TelephonyInfo()
-        t1.setNetworkCountryIso("1234")
-        t2.setNetworkCountryIso("5678")
-        result = listOf(t1,t2)
+        mTelephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        val telephonyList: List<Telephony.TelephonyInfo>
+
+        val t1 = Telephony.TelephonyInfo()
+        val t2 = Telephony.TelephonyInfo()
+        t1.networkCountryIso = "1234"
+        t2.networkCountryIso = "5678"
+        result = listOf(t1, t2)
+
+//        val telephonyList: ArrayList<HashMap<String, Any?>> = ArrayList()
+        for (i in 0 until mTelephonyManager!!.phoneCount) {
+            val data = hashMapOf<String, Any?>(
+                "carrierName" to mTelephonyManager!!.simOperatorName,
+                "dataActivity" to mTelephonyManager!!.dataActivity,
+                "radioType" to if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) radioType() else null,
+                "cellId" to cellId(),
+                "simState" to simState(i),
+                "phoneNumber" to mTelephonyManager!!.line1Number,
+                "networkOperatorName" to mTelephonyManager!!.networkOperatorName,
+                "subscriptionId" to mTelephonyManager!!.subscriptionId,
+                "isoCountryCode" to mTelephonyManager!!.simCountryIso,
+                "networkCountryIso" to mTelephonyManager!!.getNetworkCountryIso(i),
+                "mobileNetworkCode" to mTelephonyManager!!.simOperator?.substring(3),
+                "displayName" to mTelephonyManager!!.simOperatorName,
+                "mobileCountryCode" to mTelephonyManager!!.simOperator?.substring(0, 3),
+                "networkGeneration" to if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) networkGeneration() else null,
+            )
+
+            telephonyList.add(data)
+        }
         return result
     }
 }
